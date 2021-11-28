d717e7836391610c70bcc17c0720d9484c12dd4d
juniorRCJ <caldeira.junior@gmail.com>
2021-11-28 19:13:08 -0300

    apirest

---
+++ apirest/src/main/java/com/apirest/model/Cliente.java
@@ -0,0 +1,22 @@
+package com.apirest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idLong;
+	
+	@Column(nullable = false)
+	private String name;
+
+}
